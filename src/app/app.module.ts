53593167a135f28824c65829dc7741064bc706b9
MartinCifuentes <cccifuentes.martin@gmail.com>
2023-04-27 20:53:31 -0500

    feat: add header, footer and first section.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,47 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {HttpClientModule} from "@angular/common/http";
+import {LayoutModule} from "@angular/cdk/layout";
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatButtonModule} from "@angular/material/button";
+import {MatSidenavModule} from "@angular/material/sidenav";
+import {MatIconModule} from "@angular/material/icon";
+import {MatListModule} from "@angular/material/list";
+import {MatGridListModule} from "@angular/material/grid-list";
+import {MatCardModule} from "@angular/material/card";
+import {MatMenuModule} from "@angular/material/menu";
+import { FooterContentComponent } from './web/components/footer-content/footer-content.component';
+import { MainWebComponent } from './web/components/main-web/main-web.component';
+import { MainContentComponent } from './web/components/main-content/main-content.component';
+import { HeaderContentComponent } from './web/components/header-content/header-content.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterContentComponent,
+    MainWebComponent,
+    MainContentComponent,
+    HeaderContentComponent,
+    MainWebComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
+    MatGridListModule,
+    MatCardModule,
+    MatMenuModule
   ],
-  providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
---
+++ src/app/web/components/main-web/main-web.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-main-web',
+  templateUrl: './main-web.component.html',
+  styleUrls: ['./main-web.component.css']
+})
+export class MainWebComponent {
+  constructor() {
+  }
+}
