aad18e8b0f865bf27a056f98c04e9634067068b9
MartinCifuentes <cccifuentes.martin@gmail.com>
2023-06-08 14:10:17 -0500

    feat: added DesignComponent

--- src/app/design/model/shoes.ts
+++
@@ -1,7 +0,0 @@
-export interface Shoes{
-  id: number;
-  name: string;
-  description: string;
-  price: number;
-  img: string;
-}
--- src/app/design/pages/design/design.component.ts
+++ src/app/design/pages/design/design.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {ShoesServiceService} from "../../../shop/services/shoes-service.service";
 
 @Component({
   selector: 'app-design',
   templateUrl: './design.component.html',
   styleUrls: ['./design.component.css']
 })
-export class DesignComponent {
-
+export class DesignComponent implements OnInit{
+  title = 'shoes';
+  @Input() shoes: Array<any>= [];
+  selectedShoe: any;
+  showColorField: boolean = false;
+  selectedColor: string = '';
+  constructor(private shoesService: ShoesServiceService) {
+  }
+  ngOnInit(): void {
+    this.shoesService.initShoes().subscribe((data: any) => {
+      this.shoes = data;
+    });
+  }
+  onShoeSelection(shoe: any): void {
+    this.selectedShoe = shoe;
+    this.showColorField = true;
+  }
 }
