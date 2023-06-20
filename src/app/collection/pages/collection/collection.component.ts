2843fa099d13c7fcc81edb34e7ba08daae4b6a5f
JoaoBasauri <u202112272@upc.edu.pe>
2023-06-20 10:03:47 -0500

    feat: collection component and css

---
+++ src/app/collection/pages/collection/collection.component.ts
@@ -0,0 +1,13 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-collection',
+  templateUrl: './collection.component.html',
+  styleUrls: ['./collection.component.css']
+})
+export class CollectionComponent implements OnInit{
+  ngOnInit(): void {
+
+  }
+
+}
